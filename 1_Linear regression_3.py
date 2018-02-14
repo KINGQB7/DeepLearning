b405de484ed3044ad05d713cf137cbbceb511cba
KINGQB7 <cshoon421@gmail.com>
2018-02-14 21:07:41 +0900

    commit

--- 1_Linear regression_3.py
+++ 1_Linear regression_3.py
@@ -23,7 +23,7 @@ apply_gradients = optimizer.apply_gradients(gvs)
 
 # Launch the graph in a session
 sess = tf.Session()
-sess.run(tf.global_variables_initailizer())
+sess.run(tf.global_variables_initializer())
 
 for step in range(100):
     print(step, sess.run([gradient, W, gvs]))
